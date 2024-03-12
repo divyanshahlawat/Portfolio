ef4944555f3beb8d662e242f200ffc16fa582d00
Divyansh <divyanshahlwat035@gmail.com>
2024-03-13 02:14:56 +0530

    Addition of portfolio

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,62 @@
+
+function Portfolio() {
+    return (
+        <div className="bg-gradient-to-b from-black to-gray-800 text-white w-full h-full flex flex-col justify-center">
+            <div >
+                <p className="text-4xl font-bold border-b-4 inline border-gray-500">Portfolio</p>
+                <p className="pb-12 py-8">Check out some of my work here</p>
+            </div>
+            <div className="grid  sm:grid-cols-2 md:grid-cols-3  2xl:px-16 gap-8 px-12 sm:px-8 ">
+                <div className="shadow-md shadow-gray-600 rounded-lg max-w-lg">
+                    <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"  className="rounded-md duration-200 hover:scale-105" alt="" />
+                    <div className="flex justify-center items-center">
+ <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
+                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
+                    </div>
+                    
+                      
+                </div>
+                <div className="shadow-md shadow-gray-600 rounded-lg max-w-lg ">
+                    <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"  className="rounded-md duration-200 hover:scale-105" alt="" />
+                    <div className="flex justify-center items-center">
+ <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
+                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
+                    </div>
+                    
+                      
+                </div>
+                <div className="shadow-md shadow-gray-600 rounded-lg max-w-lg">
+                    <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"  className="rounded-md duration-200 hover:scale-105" alt="" />
+                    <div className="flex justify-center items-center">
+ <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
+                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
+                    </div>
+                    
+                      
+                </div>
+                <div className="shadow-md shadow-gray-600 rounded-lg max-w-lg">
+                    <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"  className="rounded-md duration-200 hover:scale-105" alt="" />
+                    <div className="flex justify-center items-center">
+ <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
+                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
+                    </div>
+                    
+                      
+                </div>
+                <div className="shadow-md shadow-gray-600 rounded-lg max-w-lg">
+                    <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"  className="rounded-md duration-200 hover:scale-105" alt="" />
+                    <div className="flex justify-center items-center">
+ <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
+                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
+                    </div>
+                    
+                      
+                </div>
+                
+                 
+            </div>  
+        </div>
+    )
+}
+
+export default Portfolio
